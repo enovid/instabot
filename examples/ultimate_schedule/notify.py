742aec5b0d1377d44959f74374e8097472f8e8e4
Emahn Novid <emahn.novid@gmail.com>
2019-03-06 02:55:00 -0800

    Create notify.py

---
+++ examples/ultimate_schedule/notify.py
@@ -0,0 +1,28 @@
+from twilio.rest import Client
+
+notify_secret = "notify_secret.txt"
+
+f = open(notify_secret)
+lines = f.readlines()
+setting_0 = lines[0].strip()
+setting_1 = lines[1].strip()
+setting_2 = lines[2].strip()
+setting_3 = lines[3].strip()
+
+account_sid = setting_0
+auth_token = setting_1
+
+client = Client(account_sid, auth_token)
+
+to_phone = setting_2
+service_phone = setting_3
+
+def sendText(msg="NA"):
+	message = client.messages.create(
+		to=to_phone,
+		from_=service_phone,
+		body=msg)
+	
+	print(message.sid)
+	
+sendText("test message")
